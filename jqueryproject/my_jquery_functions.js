b5926b6e645381016c4e09836ddd225ab30ce777
Pramod Tr-github <48082241+pramod9066@users.noreply.github.com>
2020-02-19 18:35:00 +0530

    Add files via upload

--- jqueryproject/my_jquery_functions.js
+++ jqueryproject/my_jquery_functions.js
@@ -1,17 +1,19 @@
 $(document).ready(function () {
-    var body = '{ "userNwId": "kumar" }';
-    $("#post").click(function () {
-        $.post("http://192.168.90.203:9800/nw/api/identity/user/getProfile",
-            body,
-            function (data) {
-               var json = JSON.parse(data);
-                $("#result").html(json);
-            });
+    $("#profile").click(function () {
+        var username = $("#username").val();
+        $.ajax({
+            type: 'POST',
+            url: 'http://192.168.90.203:9800/nw/api/identity/user/getProfile',
+            data: '{"userNwId":"pramod"}',
+            success: function (data) {
+                console.log(data);
+                userdetails = JSON.stringify(data);
+                $.fn.user(userdetails);               
+            },
+            contentType: "application/json",
+            dataType: 'json'
+        });
     });
-
-});
-
-
     // $.getJSON("http://192.168.90.206:9900/nw/api/utility/getNwCategoryList", function (data) {
     //     $.each(data, function (key, val) {
     //         var json = JSON.stringify(val);
@@ -19,3 +21,11 @@ $(document).ready(function () {
     //         $(".content").append(categoryNames.newsCategoryName + " ");
     //     });
     // });
+
+    $.fn.user = function(userdetails){
+        var json = JSON.parse(userdetails);
+        var username = json.nwResResult.username;
+        $("#card-title").append(username);
+    }
+});
+
