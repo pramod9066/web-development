de794c955676d4cf53efc4b1e641dbcbfeca18d7
Pramod Tr-github <48082241+pramod9066@users.noreply.github.com>
2020-02-21 15:57:45 +0530

    Add files via upload

--- jqueryproject/my_jquery_functions.js
+++ jqueryproject/my_jquery_functions.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
-        $.ajax({
-            type: 'POST',
-            url: 'http://192.168.90.203:9800/nw/api/identity/user/getProfile',
-            data: '{"userNwId":"pramod"}',
-            success: function (data) {
-                console.log(data);
-                userdetails = JSON.stringify(data);
-                $.fn.user(userdetails);
-            },
-            contentType: "application/json",
-            dataType: 'json'
-        });
+    $.ajax({
+        type: 'POST',
+        url: 'http://192.168.90.203:9800/nw/api/identity/user/getProfile',
+        data: '{"userNwId":"pramod"}',
+        success: function (data) {
+            console.log(data);
+            userdetails = JSON.stringify(data);
+            $.fn.user(userdetails);
+        },
+        contentType: "application/json",
+        dataType: 'json'
+    });
     // $.getJSON("http://192.168.90.206:9900/nw/api/utility/getNwCategoryList", function (data) {
     //     $.each(data, function (key, val) {
     //         var json = JSON.stringify(val);
@@ -30,5 +30,10 @@ $(document).ready(function () {
         $(".card-email").append(email).css("color", "black");
         $(".card-phone").append(json.nwResResult.phoneNumber).css("color", "black");
     }
+
+    $("#myBtn").click(function () {
+        $("#myModal").modal();
+    });
+
 });
 
