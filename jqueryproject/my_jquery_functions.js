52a3493214b3bc3a0a7ea9738ac69fd9c8c2e869
Pramod Tr-github <48082241+pramod9066@users.noreply.github.com>
2020-02-28 18:19:11 +0530

    Add files via upload

--- jqueryproject/my_jquery_functions.js
+++ jqueryproject/my_jquery_functions.js
@@ -4,20 +4,32 @@ $(document).ready(function () {
         url: 'http://192.168.90.203:9800/nw/api/identity/user/getProfile',
         data: '{"userNwId":"pramod"}',
         success: function (data) {
-            console.log(data);
             userdetails = JSON.stringify(data);
             $.fn.user(userdetails);
         },
         contentType: "application/json",
         dataType: 'json'
     });
-    // $.getJSON("http://192.168.90.206:9900/nw/api/utility/getNwCategoryList", function (data) {
-    //     $.each(data, function (key, val) {
-    //         var json = JSON.stringify(val);
-    //         var categoryNames = JSON.parse(json);
-    //         $(".content").append(categoryNames.newsCategoryName + " ");
-    //     });
+
+
+    // $.ajax({
+    //     type: 'POST',
+    //     url: 'http://192.168.90.206:10200/nw/api/wall/story/getStory',
+    //     data: '{"userNwId": "Pramod","storyId": "OyPWg3I2SZ"}',
+    //     success: function (data1) {
+    //         console.log(data1);
+    //     },
+    //     contentType: "application/json",
+    //     dataType: 'json'
     // });
+
+
+    $.getJSON("http://192.168.90.203:9010/nw/api/wall/v1/home/utility/getBanners",
+        function (data) {
+            var str = JSON.stringify(data);
+            var json = JSON.parse(str);
+            $.fn.bannerFunc(json);
+        });
 
     $.fn.user = function (userdetails) {
         var json = JSON.parse(userdetails);
@@ -31,9 +43,31 @@ $(document).ready(function () {
         $(".card-phone").append(json.nwResResult.phoneNumber).css("color", "black");
     }
 
+    $.fn.bannerFunc = function (bannerObject) {
+        var banner1 = bannerObject.nwResResult[0].bannerImage;
+        var bannertitle1 = bannerObject.nwResResult[0].bannerTitle;
+        var banner2 = bannerObject.nwResResult[1].bannerImage;
+        var bannertitle2 = bannerObject.nwResResult[1].bannerTitle;
+        var banner3 = bannerObject.nwResResult[2].bannerImage;
+        var bannertitle3 = bannerObject.nwResResult[2].bannerTitle;
+
+        $("#banner1").attr('src', banner1);
+        $("#bannertitle1").text(bannertitle1);
+
+        $("#banner2").attr('src', banner2);
+        $("#bannertitle2").text(bannertitle2);
+
+        $("#bannertitle3").text(bannertitle3);
+        $("#banner3").attr('src', banner3);
+    }
+
+
     $("#myBtn").click(function () {
         $("#myModal").modal();
     });
 
+    $(".nav-tabs a").click(function () {
+        $(this).tab('show');
+    });
 });
 
