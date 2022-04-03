f15e8add77114824fea12a82d6bd85ef4d460cb9
Pramod Tr-github <48082241+pramod9066@users.noreply.github.com>
2022-04-03 19:49:04 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello batch25 team");
-      System.out.println("Hello batch25 students and training practice with Pramod checking Continous intergtaion 2 ");
+      System.out.println("Hello batch25 students and training practice with Pramod checking Continous intergtaion 3 its working ");
     }  
 }  
